5e556773dd4bcb28eff580b03179767f9290469d
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-02-07 08:34:56 +0100

    * made header <link> and <meta> elements XHTML compliant

--- header.php
+++ header.php
@@ -4,15 +4,15 @@
 	<head>
 
 		<!-- "H5": The HTML-5 WordPress Template Theme -->
-		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
+		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>"/>
 		<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<meta name="description" content="<?php bloginfo('description'); ?>"/>
 
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen">
-		<link rel="alternate" type="text/xml" title="<?php bloginfo('name'); ?> RSS 0.92 Feed" href="<?php bloginfo('rss_url'); ?>">
-		<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>">
-		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS 2.0 Feed" href="<?php bloginfo('rss2_url'); ?>">
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen"/>
+		<link rel="alternate" type="text/xml" title="<?php bloginfo('name'); ?> RSS 0.92 Feed" href="<?php bloginfo('rss_url'); ?>"/>
+		<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>"/>
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS 2.0 Feed" href="<?php bloginfo('rss2_url'); ?>"/>
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
 
 		<?php wp_enqueue_script('jquery'); ?>
 		<?php wp_head(); ?>
