5f0f07639213173404d41afb937e9bbc9dc71fed
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-02-09 06:04:19 +0100

    * changed RSS feeds to ATOM in footer

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
             <p>
                 © <?= date('Y'); ?> <a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
                 XH5 Theme für <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a>
-                <a href="<?php bloginfo('rss2_url'); ?>" title="Blogartikel als Feed abonnieren">Artikelfeed (RSS)</a>
-                <a href="<?php bloginfo('comments_rss2_url'); ?>" title="Kommentare als Feed abonnieren">Kommentarfeed (RSS)</a>
+                <a href="<?php bloginfo('atom_url'); ?>" title="Blogartikel als Feed abonnieren">Artikelfeed (ATOM)</a>
+                <a href="<?php bloginfo('comments_atom_url'); ?>" title="Kommentare als Feed abonnieren">Kommentarfeed (ATOM)</a>
             </p>
         </footer>
         <?php wp_footer(); ?>
