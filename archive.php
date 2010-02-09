b34fb0f33b1529a17e2df2c8f2414bcb941cf598
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-02-09 02:22:59 +0100

    * fixed entity problem, translated stuff

--- archive.php
+++ archive.php
@@ -6,25 +6,25 @@
 
             <?php $post = $posts[0]; // hack: set $post so that the_date() works ?>
             <?php if (is_category()) { ?>
-            <h1>Archive for the &ldquo;<?php single_cat_title(); ?>&rdquo; Category</h1>
+            <h1>Archiv für Kategorie „<?php single_cat_title(); ?>“</h1>
 
             <?php } elseif(is_tag()) { ?>
-            <h1>Posts Tagged &ldquo;<?php single_tag_title(); ?>&rdquo;</h1>
+            <h1>Archiv für Tag „<?php single_tag_title(); ?>“</h1>
 
             <?php } elseif (is_day()) { ?>
-            <h1>Archive for <?php the_time('F jS, Y'); ?></h1>
+            <h1>Archiv für <?php the_time('j. F Y'); ?></h1>
 
             <?php } elseif (is_month()) { ?>
-            <h1>Archive for <?php the_time('F, Y'); ?></h1>
+            <h1>Archiv für <?php the_time('F Y'); ?></h1>
 
             <?php } elseif (is_year()) { ?>
-            <h1>Archive for <?php the_time('Y'); ?></h1>
+            <h1>Archiv für <?php the_time('Y'); ?></h1>
 
             <?php } elseif (is_author()) { ?>
-            <h1>Author Archive</h1>
+            <h1>Autor-Archiv</h1>
 
             <?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-            <h1>Blog Archives</h1>
+            <h1>Blog-Archiv</h1>
 
         <?php } ?>
         <?php while (have_posts()) : the_post(); ?>
