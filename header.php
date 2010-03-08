7d0fd9b63b1cd08d5153b2d79360d04bbc958adf
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-03-08 16:17:42 +0100

    * changed Content-Type to text/html to accomodate for existing tag soup content

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
-<?php header("Content-Type: application/xhtml+xml; charset=utf-8") ?>
+<?php header("Content-Type: text/html; charset=utf-8") ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>>
 	<head>
 
 		<!-- "XH5": The XHTML-5 WordPress Template Theme -->
