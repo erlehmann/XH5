b4c87667682d906a14a613a356601d8975364e17
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-03-22 02:17:00 +0100

    - RSS feed links (only ATOM feed remains)

--- header.php
+++ header.php
@@ -8,12 +8,9 @@
         <meta name="description" content="<?php bloginfo('description'); ?>"/>
 
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen"/>
-        <link rel="alternate" type="text/xml" title="<?php bloginfo('name'); ?> RSS 0.92 Feed" href="<?php bloginfo('rss_url'); ?>"/>
         <link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>"/>
-        <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS 2.0 Feed" href="<?php bloginfo('rss2_url'); ?>"/>
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
 
-        <?php wp_enqueue_script('jquery'); ?>
         <?php wp_head(); ?>
 
         <script src="<?php bloginfo('template_directory'); ?>/javascript/h5.js"></script>
