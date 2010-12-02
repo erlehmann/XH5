898876863fd83105bcb984fb57b5082283ad966f
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-12-02 22:22:02 +0100

    - more getting rid of implicit elements

--- footer.php
+++ footer.php
@@ -1,12 +1,9 @@
-        <footer>
-            <p>
-                © <?= date('Y'); ?> <a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
-                XH5 Theme für <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a>
-                <a href="<?php bloginfo('atom_url'); ?>" title="Blogartikel als Feed abonnieren">Artikelfeed (ATOM)</a>
-                <a href="<?php bloginfo('comments_atom_url'); ?>" title="Kommentare als Feed abonnieren">Kommentarfeed (ATOM)</a>
-            </p>
-        </footer>
-        <?php wp_footer(); ?>
-
-    </body>
-</html>
+<footer>
+    <p>
+        © <?= date('Y'); ?> <a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
+        XH5 Theme für <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a>
+        <a href="<?php bloginfo('atom_url'); ?>" title="Blogartikel als Feed abonnieren">Artikelfeed (ATOM)</a>
+        <a href="<?php bloginfo('comments_atom_url'); ?>" title="Kommentare als Feed abonnieren">Kommentarfeed (ATOM)</a>
+    </p>
+</footer>
+<?php wp_footer(); ?>
