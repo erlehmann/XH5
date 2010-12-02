18c0589e870a024c23054dc68e502d1bcd1cc315
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-12-02 22:28:00 +0100

    * clear up footer

--- footer.php
+++ footer.php
@@ -1,9 +1,4 @@
 <footer>
-    <p>
-        © <?= date('Y'); ?> <a href="<?php bloginfo('url'); ?>/" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
-        XH5 Theme für <a href="http://wordpress.org/">WordPress <?php bloginfo('version'); ?></a>
-        <a href="<?php bloginfo('atom_url'); ?>" title="Blogartikel als Feed abonnieren">Artikelfeed (ATOM)</a>
-        <a href="<?php bloginfo('comments_atom_url'); ?>" title="Kommentare als Feed abonnieren">Kommentarfeed (ATOM)</a>
-    </p>
+    <a href="https://github.com/erlehmann/XH5">XH5</a> für <a href="http://wordpress.org/">WordPress</a>
 </footer>
 <?php wp_footer(); ?>
