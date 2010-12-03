7f45b9335ff87408e17d8e5e63c2f7c4f0bfae22
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-12-03 23:18:41 +0100

    * sidebar needs to be before bottom navigation

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
+<?php get_sidebar(); ?>
 <nav>
     <h1>Weitere Artikel</h1>
     <?php posts_nav_link(' '); ?>
 </nav>
-<?php get_sidebar(); ?>
 <footer>
     <a href="https://github.com/erlehmann/XH5">XH5</a> für <a href="http://wordpress.org/">WordPress</a>
 </footer>
