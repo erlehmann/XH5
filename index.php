2fc943decb4332093e8ac3b6b9274a44cfc720e5
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-12-13 02:53:58 +0100

    * section and div with section class for consistent styling

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
+<section>
+<h1>Neueste Artikel</h1>
 <?php while (have_posts()) : the_post(); ?><?php include 'article.php'; ?><?php endwhile; ?>
+</section>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,28 +1,17 @@
 <?php get_header(); ?>
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-<article id="post-<?php the_ID(); ?>">
-    <?php include 'article-content.php'; ?>
-    <?php comments_template(); ?>
-</article>
-
-<nav>
-    <p>
-        <?php previous_post_link(); ?><?php next_post_link(); ?>
-    </p>
-</nav>
-
-    <?php endwhile; else: ?>
-
-<article>
-    <p>
-        Kein Artikel entsprach den gewünschten Kriterien.
-    </p>
-</article>
-
-    <?php endif; ?>
-
-<?php get_sidebar(); ?>
+<?php while (have_posts()) : the_post(); ?>
+<div class="section">
+    <article id="post-<?php the_ID(); ?>">
+        <?php include 'article-content.php'; ?>
+        <nav>
+            <h3>Weitere Artikel</h3>
+            <?php previous_post_link(); ?>
+            <?php next_post_link(); ?>
+        </nav>
+        <?php comments_template(); ?>
+    </article>
+</div>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
